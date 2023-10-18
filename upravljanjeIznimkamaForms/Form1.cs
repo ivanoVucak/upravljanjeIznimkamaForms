8d8b2c10ace237087e5b9edee7cf7c80c3cceb36
ivanoVucak <145533812+ivanoVucak@users.noreply.github.com>
2023-10-18 09:02:18 +0200

    Add files via upload

---
+++ upravljanjeIznimkamaForms/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace upravljanjeIznimkamaForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ispitTxt_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void unesiBtn_Click(object sender, EventArgs e)
+        {
+            try {
+                int godRod = Convert.ToInt16(upisTxt.Text);
+                ispisTxt.Text = godRod.ToString();
+                upisTxt.Clear();
+            }
+
+            catch (Exception greska) {
+                DialogResult odabir = MessageBox.Show("Molim vas da upišete broj.\nDa li želite izbrisati vaš postojeći unos","Pogrešan unos", MessageBoxButtons.YesNo, MessageBoxIcon.Error);
+
+                switch (odabir) { 
+                    case DialogResult.Yes:
+                        upisTxt.Clear(); break;
+                    case DialogResult.No:
+                        break;
+                
+                }
+
+                upisTxt.Select();
+            }
+
+        }
+    }
+}
